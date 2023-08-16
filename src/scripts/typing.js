45d4004634faac20301d50b91e21ab5b1bbe1f03
omkarxpatel <bendonveryt@gmail.com>
2023-08-16 09:11:00 -0700

    update

--- src/scripts/typing.js
+++ src/scripts/typing.js
@@ -1,33 +1,41 @@
 const title = document.querySelector('.home-text h2');
 const text = ["Highschool Student", "Coder", "Musician", "Martial Artist"];
 let b = 0;
+let i = 0;
+let isTyping = true;
 
 function type() {
-  let i = 0;
-  const intervalId = setInterval(() => {
+  if (isTyping) {
     title.textContent += text[b].charAt(i);
     i++;
-    if (i >= text[b].length) {
-        b = (b + 1) % text.length;
 
-        clearInterval(intervalId);
-        setTimeout(() => {
-            deleteText();
-        }, 1000);
+    if (i >= text[b].length) {
+      isTyping = false;
+      setTimeout(() => {
+        deleteText();
+      }, 1000);
     }
-  }, 100);
+  }
 }
 
 function deleteText() {
-  const intervalId = setInterval(() => {
-    title.textContent = title.textContent.slice(0, -1);
-    if (title.textContent.length === 1) {
-      clearInterval(intervalId);
+  if (!isTyping) {
+    if (title.textContent.length > 0) {
+      title.textContent = title.textContent.slice(0, -1);
+    } else {
+      b = (b + 1) % text.length;
+      i = 0;
+      isTyping = true;
       setTimeout(() => {
         type();
       }, 1000);
     }
-  }, 75);
+  }
 }
 
+setInterval(() => {
+  type();
+}, 100);
+
+// Initial call to start typing
 type();
